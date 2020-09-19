338fe315cda0e5418edccd7413b3ff9c91017b81
clarissa <clarissah99@gmail.com>
2020-09-19 01:32:38 -0400

    updated groceryitem class

--- Project1/src/GroceryItem.java
+++ Project1/src/GroceryItem.java
@@ -1,2 +1,22 @@
 public class GroceryItem {
+    private String name;
+    private double price;
+    private boolean taxable;
+
+    public GroceryItem (String name, double price, boolean taxable) {
+        this.name = name;
+        this.price = price;
+        this.taxable = taxable;
+    }
+
+    public boolean equals (Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString () {
+        return name + ": $" + price + " : " + taxable;
+    }
 }
