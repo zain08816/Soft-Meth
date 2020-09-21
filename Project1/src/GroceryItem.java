6a3d9693a96d3e40393bef885eb44a0b9827eebb
clarissa <clarissah99@gmail.com>
2020-09-20 20:16:00 -0400

    updated GroceryItem to what you put lol

--- Project1/src/GroceryItem.java
+++ Project1/src/GroceryItem.java
@@ -3,20 +3,55 @@ public class GroceryItem {
     private double price;
     private boolean taxable;
 
+    /**
+     * @param name
+     * @param price
+     * @param taxable
+     */
     public GroceryItem (String name, double price, boolean taxable) {
         this.name = name;
         this.price = price;
         this.taxable = taxable;
     }
 
+    /**
+     *
+     * @return
+     */
+    public double price () {
+        return this.price;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isTaxable () {
+        return this.taxable;
+    }
+
+    /**
+     *
+     * @param obj
+     * @return
+     */
     public boolean equals (Object obj) {
         if (this == obj) {
             return true;
         }
+
+        GroceryItem compare = (GroceryItem) obj;
+
+        if (this.name.equals(compare.name) && this.price == compare.price && this.taxable == compare.taxable) {
+            return true;
+        }
+
         return false;
     }
 
     public String toString () {
-        return name + ": $" + price + " : " + taxable;
+        String tax = taxable ? "is taxable" : "tax free";
+        return String.format("%s: $%.2f : %s", name, price, tax);
     }
+
 }
